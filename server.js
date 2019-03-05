45e09b4d1fa1955bf1e437bc502c97e7c1031ba9
jxa8hst <jean_altidor@homdepot.com>
2019-03-04 20:54:37 -0500

    graql backend query and create event working

--- server.js
+++ server.js
@@ -1,17 +1,48 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-const jean = 'Jean'
-app.get('/', (req,res, next) => {
-      res.send(jean)
-})
+// const jean = 'Jean'
 
-const PORT = process.env.PORT || 3000
-app.listen( PORT, ()=> {
-      console.log(`You are now running on port ${ PORT }`);
-})
+const schema = buildSchema(`
+      type RootQuery {
+            events: [ String! ]
+      }
+
+      type RootMutation {
+            createEvent(name: String): String
+      }
+      schema {
+            query: RootQuery
+            mutation: RootMutation
+      }
+
+`);
+
+const root = {
+  events: () => {
+    return ["Cooking", "Sailing", "flippers"];
+  },
+  createEvent: args => {
+    const eventName = args.name;
+    return eventName;
+  }
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`You are now running on port ${PORT}`);
+});
