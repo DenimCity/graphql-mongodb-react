af96808865d4a34df4e9b357ce1c8727093df4fe
jxa8hst <jean_altidor@homdepot.com>
2019-03-04 23:09:13 -0500

    setting up mongodb atlas

--- server.js
+++ server.js
@@ -2,19 +2,34 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHTTP = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require('mongoose');
 
+const Event = require('./model/event');
 const app = express();
 app.use(bodyParser.json());
 
-// const jean = 'Jean'
 
 const schema = buildSchema(`
+      type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+      }
       type RootQuery {
-            events: [ String! ]
+            events: [ Event! ]!
+      }
+
+      input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
       }
 
       type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
       }
       schema {
             query: RootQuery
@@ -25,11 +40,23 @@ const schema = buildSchema(`
 
 const root = {
   events: () => {
-    return ["Cooking", "Sailing", "flippers"];
+    return events;
   },
   createEvent: args => {
-    const eventName = args.name;
-    return eventName;
+       const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: new Date(args.eventInput.date)
+       })
+       return event.save().then(result => {
+              console.log(result)
+              return {...result._doc}
+        }).catch(error => {
+              console.log(error)
+              throw err;
+        })
+    return event;
   }
 };
 
@@ -38,11 +65,19 @@ app.use(
   graphqlHTTP({
     schema: schema,
     rootValue: root,
-    graphiql: true
+    graphiql: true,
+    pretty: true
   })
 );
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`You are now running on port ${PORT}`);
-});
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD }@cluster0-unydg.mongodb.net/${ process.env.MONGO_DB }?retryWrites=true`)
+.then(()=> {
+      const PORT = process.env.PORT || 3000;
+      app.listen(PORT, () => {
+        console.log(`You are now running on port ${PORT}`);
+      });
+
+})
+.catch(err => {
+      console.log(err);
+})
