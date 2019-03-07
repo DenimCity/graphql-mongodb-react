2fab4c1d592ccc855fedcf576e8c09c099b4155f
jxa8hst <jean_altidor@homdepot.com>
2019-03-07 08:25:28 -0500

    added react as my front end

---
+++ client/src/components/Booklist.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Booklist extends Component {
+  render() {
+    return (
+      <div>
+        <ul id='book-list'>
+        <li>Book Name</li>
+        </ul>
+      </div>
+    )
+  }
+}
--- server.js
+++ server.js
@@ -54,17 +54,9 @@ app.use(
   })
 );
 
-// mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD }@cluster0-unydg.mongodb.net/${ process.env.MONGO_DB }?retryWrites=true`)
-// .then(()=> {
-     
-
-// })
-// .catch(err => {
-//       console.log(err);
-// })
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`You are now running on port ${PORT}`);
 });
