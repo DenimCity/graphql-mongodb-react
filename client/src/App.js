aee85e1706292800a1a4336fb5d8676d90fe3237
jxa8hst <jean_altidor@homdepot.com>
2019-03-07 08:33:31 -0500

    setup and intergrated graphql apollo and apolloprovider

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider }  from 'react-apollo';
+
 import BookList from './components/Booklist';
 
 
+
+
+const client = new ApolloClient({
+  ur: 'http://localhost:4000/graphql',
+
+})
 class App extends Component {
   render() {
     return (
-      <div id="main">
+     <ApolloProvider client={client}>
+        <div id="main">
         <h1>Jeans Reading List</h1>
         <BookList/>
       </div>
+     </ApolloProvider>
     );
   }
 }
